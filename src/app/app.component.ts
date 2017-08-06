0de7f7cba19ea6752f9e2a8158f0e2089ea8b8f8
zzimickQ <zzimick@gmail.com>
2017-08-07 02:38:48 +0300

    navbar done ui coolinized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component , ViewEncapsulation} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./css/theme.css','./css/vendor/material-icons.min.css','./css/vendor/socicon.min.css'],
+  styleUrls: [
+    './css/vendor/bs/bootstrap.min.css',
+    './css/vendor/socicon.min.css',
+    './css/my_theme.css',
+    './css/vendor/font-awesome/css/font-awesome.min.css'
+  ],
    encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
 }
+
