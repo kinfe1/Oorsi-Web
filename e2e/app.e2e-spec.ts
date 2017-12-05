d703291300a8bdddef0c758049dc00011e447322
Anteneh Negash <antenehnegash@gmail.com>
2017-12-05 00:10:52 -0500

    package upgrade

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { PROJECTNAMEPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('project-name App', () => {
-  let page: PROJECTNAMEPage;
+describe('oorsi-web App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new PROJECTNAMEPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
