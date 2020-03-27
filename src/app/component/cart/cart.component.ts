39b9bffdbd06fabe546be16a1a60b7f593abea09
Zemichael Tesfaye <zzimick@gmail.com>
2020-03-27 23:33:33 +0300

    cart increment / decrement UI fix. only

--- src/app/component/cart/cart.component.ts
+++ src/app/component/cart/cart.component.ts
@@ -1,52 +1,66 @@
-import { AuthService } from './../../service/auth/auth.service';
-import { CartProduct } from '../../model/cartProduct';
-import { CartService } from './../../service/cart/cart.service';
-import { Component, OnInit } from '@angular/core';
-import { User } from '../../model/user';
+import { AuthService } from "./../../service/auth/auth.service";
+import { CartProduct } from "../../model/cartProduct";
+import { CartService } from "./../../service/cart/cart.service";
+import { Component, OnInit } from "@angular/core";
+import { User } from "../../model/user";
 
 @Component({
-  selector: 'oorsi-web-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  selector: "oorsi-web-cart",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.css"]
 })
 export class CartComponent implements OnInit {
-
   users: User[] = [];
 
-  constructor(private cartService: CartService, private authService: AuthService) { }
+  constructor(
+    private cartService: CartService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
-    this.cartService.getCart().subscribe(data => {
-      let cartProducts: CartProduct[] = data;
-      for (let cartProduct of cartProducts) {
-        let added: boolean = false;
-        for (let user of this.users) {
-          if (cartProduct.forUser.userID === user.userID) {
+    this.cartService.getCart().subscribe(
+      data => {
+        let cartProducts: CartProduct[] = data;
+        for (let cartProduct of cartProducts) {
+          let added: boolean = false;
+          for (let user of this.users) {
+            if (cartProduct.forUser.userID === user.userID) {
+              cartProduct.forUser = user;
+              user.cartProducts.push(cartProduct);
+              added = true;
+              break;
+            }
+          }
+
+          if (!added) {
+            let user: User = new User(cartProduct.forUser);
             cartProduct.forUser = user;
             user.cartProducts.push(cartProduct);
-            added = true;
-            break;
+            this.users.push(user);
           }
         }
-
-        if (!added) {
-          let user: User = new User(cartProduct.forUser);
-          cartProduct.forUser = user;
-          user.cartProducts.push(cartProduct);
-          this.users.push(user);
-        }
-
-      }
-    }, err => this.authService.checkError(err));
-
+      },
+      err => this.authService.checkError(err)
+    );
   }
 
   onDeleteCartProduct(cartProduct: CartProduct) {
-    this.cartService.deleteCartProduct(cartProduct).then(data => {
-      cartProduct.forUser.cartProducts.splice(cartProduct.forUser.cartProducts.indexOf(cartProduct), 1);
-    }, err => this.authService.checkError(err));
+    this.cartService.deleteCartProduct(cartProduct).then(
+      data => {
+        cartProduct.forUser.cartProducts.splice(
+          cartProduct.forUser.cartProducts.indexOf(cartProduct),
+          1
+        );
+      },
+      err => this.authService.checkError(err)
+    );
   }
 
+  decrease(cartProduct: CartProduct) {
+    cartProduct.quantity--;
+  }
+  increase(cartProduct: CartProduct) {
+    cartProduct.quantity++;
 
-
+  }
 }
