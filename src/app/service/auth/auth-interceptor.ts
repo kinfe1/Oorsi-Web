21b17b2a39fccf7a9cfc800e232f1e8ef4d65504
Zemichael Tesfaye <zzimick@gmail.com>
2020-04-11 09:26:33 +0300

    fix - logout user when unauthorized response is received. and cleared requests in the navigation component that needs to check user is logged in before performing those requests.

--- src/app/service/auth/auth-interceptor.ts
+++ src/app/service/auth/auth-interceptor.ts
@@ -1,6 +1,15 @@
-import { HttpClient, HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpErrorResponse,
+} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { catchError } from "rxjs/operators";
+import { throwError, of } from "rxjs";
+import { Router } from "@angular/router";
 
 /*
   Generated class for the AuthInterceptorProvider provider.
@@ -10,24 +19,59 @@ import { Observable } from 'rxjs/Observable';
 */
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
-  constructor() {
-    console.log('Hello AuthInterceptor');
+  constructor(private router: Router) {
+    console.log("Hello AuthInterceptor");
   }
 
-
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let token: string = localStorage.getItem('currentUser');
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    let token: string = localStorage.getItem("currentUser");
+    let pipedNext;
     if (token) {
       const authReq = request.clone({
         setHeaders: {
-          Authorization: "Bearer " + token
-        }
+          Authorization: "Bearer " + token,
+        },
       });
-      return next.handle(authReq)
+      pipedNext = next.handle(authReq);
     } else {
-      return next.handle(request)
+      pipedNext = next.handle(request);
+    }
+    pipedNext = pipedNext.pipe(
+      // pipe all errors to get unauthorized error and logout user
+      catchError((err: any) => {
+        console.log("http interceptor", err);
+        if (err instanceof HttpErrorResponse) {
+          // check if error status is unauthorized (401)
+          if (err.status == 401) {
+            console.log("Unauthorized response received at HTTP Interceptor");
+            this.handleUnauthorizedResponse();
+          }
+        }
+        // pipe error to the observer
+        return throwError(err);
+      })
+    );
+    return pipedNext;
+  }
+
+  /**
+   * method called when an unauthorized response is received to logout
+   * the user and route to login.
+   */
+  handleUnauthorizedResponse() {
+    // remove token
+    localStorage.removeItem("currentUser");
+
+    // check if route is already at login
+    // so no redirect LOOP is not produced.
+    if(this.router.isActive('/login', true)) {
+      return;
     }
 
+    // go to login page
+    this.router.navigateByUrl("/login");
   }
 }
