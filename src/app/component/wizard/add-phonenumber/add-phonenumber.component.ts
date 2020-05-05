6f6aece0d572f04b1a3d4acc32c1f2cd11a2e8c4
Zemichael Tesfaye <zzimick@gmail.com>
2020-05-05 11:06:02 +0300

    phone number view imple

--- src/app/component/wizard/add-phonenumber/add-phonenumber.component.ts
+++ src/app/component/wizard/add-phonenumber/add-phonenumber.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-add-birthday',
-  templateUrl: './add-phonenumber.component.html',
-  styleUrls: ['./add-phonenumber.component.css']
+  selector: "app-add-phonenumber",
+  templateUrl: "./add-phonenumber.component.html",
+  styleUrls: ["./add-phonenumber.component.css"],
 })
 export class AddPhonenumberComponent implements OnInit {
+  submitted = false;
 
-  constructor() { }
+  phoneNumberFormGroup: FormGroup;
 
-  ngOnInit(): void {
+  constructor(fb: FormBuilder) {
+    this.phoneNumberFormGroup = fb.group({
+      phoneNumber: ["", [Validators.required]],
+      verifyCode: ["", [Validators.required]],
+    });
   }
 
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log("submitted");
+    this.phoneNumberFormGroup.controls.phoneNumber.markAsTouched();
+    console.log(this.phoneNumberFormGroup.value);
+    console.log(this.phoneNumberFormGroup.controls.phoneNumber.errors);
+
+    if (
+      this.phoneNumberFormGroup.controls.phoneNumber.valid &&
+      this.phoneNumberFormGroup.controls.verifyCode.valid
+    ) {
+      // send verifivation code to server to complete this form
+
+      return;
+    }
+
+    if (this.phoneNumberFormGroup.controls.phoneNumber.valid) {
+      // perform phone verification and
+      // mark submitted to true if correct
+      this.submitted = true;
+    }
+  }
 }
